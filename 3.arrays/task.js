f710d30d0f7e5ce261ceaa0335fe9b76b2673b9f
Your Name <you@example.com>
2021-12-15 19:53:59 +0700

    add task arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
-function compareArrays(arr1, arr2) {
-  let result;
+"use strict";
 
-  // Ваш код
+function compareArrays(arr1, arr2) {
+  let result = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter((number) => number > 0 && number % 3 === 0).map((number) => number * 10);
 
   return resultArr; // array
 }
