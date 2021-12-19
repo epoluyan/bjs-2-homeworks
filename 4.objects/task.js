ad5d00cfdbf6d38936b35599283dfc37577140ea
Your Name <you@example.com>
2021-12-19 10:55:28 +0700

    add task objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,29 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  this.marks === undefined ? this.marks[0] = mark  : this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...mark) {
+  mark.forEach(element => this.marks.push(element));
+}
+
+Student.prototype.getAverage = function () {
+  const sum = (previousValue, currentValue) => previousValue + currentValue;
+  return this.marks.reduce(sum) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete Student.subject;
+  delete Student.marks;
+  this.excluded = reason;
+}
